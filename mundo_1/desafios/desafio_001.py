9e0bc4ddda4a01331ee1af1d63aeac441b57fd33
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-01 08:46:32 -0300

    Desafio 001.
    Nesse desafio, o algoritmo pede o nome do usuário e retorna formatado uma mensagem de boas-vindas.
    Como eu já tenho uma noção de Python optei em usar a formatação(format) ao invés do + de concatenação.

---
+++ mundo_1/desafios/desafio_001.py
@@ -0,0 +1,4 @@
+# Crie um script Python que leia o nome da pessoa e mostre uma mensagem de boas-vindas com o valor digitado.
+
+nome_usuario = input('Digite o seu nome: ')
+print(f'Olá,{nome_usuario}! Prazer em te conhecer!')
