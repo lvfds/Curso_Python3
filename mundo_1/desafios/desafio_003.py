106a5b1f93def2e95ac3ebbd5928fd3eb2d4def2
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-01 09:01:09 -0300

    Desafio 003.
    Nesse desafio é pedido que o algoritmo peça ao usuário dois números e mostre a soma.
    No desafio está escrito TENTE pois ele não mostrou na aula a função de converter String para INT, como eu já sabia que iria apenas concatenar resolvi logo fazer a conversão.

---
+++ mundo_1/desafios/desafio_003.py
@@ -0,0 +1,6 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+numero_1 = int(input('Digite o 1° número: '))
+numero_2 = int(input('Digite o 2° número: '))
+soma_dos_numeros = numero_1 + numero_2
+print(f'A soma é {soma_dos_numeros}.')
