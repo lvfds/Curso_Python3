6f2bab1fcf6903c23535788e26704e394e1bef22
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-03 08:01:49 -0300

    Desafio 009.
    Nesse desafio foi proposto fazer a tabuada de qualquer número que seja digitado.
    Eu sei que poderia ser feito com o While ou For mas como o curso não chegou nessa parte ainda, preferi seguir o seu cronograma.

---
+++ mundo_1/desafios/desafio_009.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+numero_digitado = int(input('Digite um número: '))
+print(f'{numero_digitado} x 1 = {(numero_digitado * 1)}')
+print(f'{numero_digitado} x 2 = {(numero_digitado * 2)}')
+print(f'{numero_digitado} x 3 = {(numero_digitado * 3)}')
+print(f'{numero_digitado} x 4 = {(numero_digitado * 4)}')
+print(f'{numero_digitado} x 5 = {(numero_digitado * 5)}')
+print(f'{numero_digitado} x 6 = {(numero_digitado * 6)}')
+print(f'{numero_digitado} x 7 = {(numero_digitado * 7)}')
+print(f'{numero_digitado} x 8 = {(numero_digitado * 8)}')
+print(f'{numero_digitado} x 9 = {(numero_digitado * 9)}')
+print(f'{numero_digitado} x 10 = {(numero_digitado *10 )}')
