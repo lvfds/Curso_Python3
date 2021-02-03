f3768bd22d7248678bf68e0c956b96733af81901
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-03 07:12:09 -0300

    Desafio 006.
    Nesse desafio, pegamos um número digitado pelo usuário, retornamos o dobro, triplo e raiz quadrada.

---
+++ mundo_1/desafios/desafio_006.py
@@ -0,0 +1,4 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+numero_digitado = int(input('Digite um número: '))
+print(f'O dobro do número {numero_digitado} é {(numero_digitado * 2)}, o seu triplo é {(numero_digitado * 3)} e sua raiz quadrada é {(numero_digitado ** 0.5)}.')
