25a69ec6c728b9c2974adae47f03a1a0ab63d300
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-03 06:56:45 -0300

    Desafio 005.
    Nesse desafio foi proposto pegar um número digitado pelo usuário e mostrar seu sucessor e antecessor.

---
+++ mundo_1/desafios/desafio_005.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+numero_digitado = int(input('Digite um número: '))
+print(f'O sucessor do número {numero_digitado} é {(numero_digitado + 1)}, e seu antecessor é {(numero_digitado - 1)}.')
