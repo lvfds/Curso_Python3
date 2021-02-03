b1223026b20daa45faaafc32005b6b6f810042bd
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-03 07:26:36 -0300

    Desafio 007.
    Nesse desafio foi pedido para pedir ao usuário 2 notas e mostrar a média delas.

---
+++ mundo_1/desafios/desafio_007.py
@@ -0,0 +1,6 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+
+nota1 = float(input('Digite a 1ª nota: '))
+nota2 = float(input('Digite a 2ª nota: '))
+media = (nota1+nota2) / 2
+print(f'A sua média foi de {media}.')
