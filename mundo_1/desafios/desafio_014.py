da1a30412a62501d5faf6e93cd47021b0643102a
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-04 06:36:22 -0300

    Adição do enunciado.
    Tinha esquecido de coloar o enunciado do desafio como comentário no código.

--- mundo_1/desafios/desafio_014.py
+++ mundo_1/desafios/desafio_014.py
@@ -1,3 +1,5 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+
 temperatura_em_celsius = float(input('Informe a temperatura em C°: '))
 temperatura_em_fahrenheit = (temperatura_em_celsius * 9/5) + 32
 print(f'A temperatura de {temperatura_em_celsius:.1f}°C corresponde a {temperatura_em_fahrenheit:.1f}°F.')
