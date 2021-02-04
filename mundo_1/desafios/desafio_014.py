fc6e6cb7987667fc1c2726b870fbb54c25474ad3
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-04 06:34:05 -0300

    Desafio 014.
    Nesse dasafio fiz uma conversão básica de temperaturas.

---
+++ mundo_1/desafios/desafio_014.py
@@ -0,0 +1,3 @@
+temperatura_em_celsius = float(input('Informe a temperatura em C°: '))
+temperatura_em_fahrenheit = (temperatura_em_celsius * 9/5) + 32
+print(f'A temperatura de {temperatura_em_celsius:.1f}°C corresponde a {temperatura_em_fahrenheit:.1f}°F.')
