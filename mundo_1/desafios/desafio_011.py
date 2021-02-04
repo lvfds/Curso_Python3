e0215375f3e1a576aae0dfc616bae818b882a4c1
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-04 05:54:58 -0300

    Corrigindo erro.
    O erro não foi de sintaxe mas sim eu troquei a variável 'area_da_parede' por 'altura_da_parede' na exibição do código.

--- mundo_1/desafios/desafio_011.py
+++ mundo_1/desafios/desafio_011.py
@@ -6,4 +6,4 @@ largura_da_parede = float(input('Digite a largura da parede em m²: '))
 altura_da_parede = float(input('Digite a altura da parede em m²: '))
 area_da_parede = largura_da_parede * altura_da_parede
 quantidade_de_litros_de_tinta = area_da_parede / 2
-print(f'Com a área da parede de {altura_da_parede}m², será nescessário {quantidade_de_litros_de_tinta}L de tinta.')
+print(f'Com a área da parede de {area_da_parede}m², será nescessário {quantidade_de_litros_de_tinta}L de tinta.')
