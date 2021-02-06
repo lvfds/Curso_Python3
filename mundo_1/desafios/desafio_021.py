41f1db5db33755fae6d904fe6adfc1d7719c3b0f
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-06 08:21:34 -0300

    Desafio 021.
    Nesse desafio foi proposto reproduzir um arquivo MP3 que estiver no seu diretório.
    Coloquei um comentário de aviso sobre o diretório pois eu estava testando os módulos e nenhum deles funcionava por quê o Windows copia o PATH do arquivo sem a segunda barra.

---
+++ mundo_1/desafios/desafio_021.py
@@ -0,0 +1,7 @@
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+# Créditos pelo arquivo de áudio MP3: https://www.w3schools.com/html/horse.mp3
+# OBS: o arquivo de áudio é apenas reproduzido quando tem DUAS BARRAS no diretório principal exemplo: C://, D:// ...
+
+from playsound import playsound
+
+playsound('C:\\Users\lucas\Documents\Curso_Python3\mundo_1\desafios\horse.mp3')
