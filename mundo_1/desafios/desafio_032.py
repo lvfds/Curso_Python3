5d60234e30f65df367920f915569725a3e0c4fb4
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-10 07:42:48 -0300

    Atualização desafio 032.
    Eu só atualizei a formula, pois como o Guanabara disse a forma clássica é apenas pegar o resto da divisão por 4 mas anos antes da adição do ano bissexto ficariam dando caso continuasse usando apenas a formula clássica.

--- mundo_1/desafios/desafio_032.py
+++ mundo_1/desafios/desafio_032.py
@@ -1,7 +1,7 @@
 # Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
 
 ano_digitado = int(input('Digite um ano qualquer: '))
-if ano_digitado %4 == 0:
+if ano_digitado %4 == 0 and ano_digitado %100 != 0 or ano_digitado % 400 == 0:
     print(f'O ano de {ano_digitado} é BISSEXTO.')
 else:
     print(f'O ano de {ano_digitado} não é BISSEXTO.')
