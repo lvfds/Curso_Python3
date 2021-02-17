94241aef2c27ed4ceb82dbd9a5d8fc6418b03b2a
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-17 08:35:39 -0300

    Desafio 053.
    Nesse desafio foi proposto verificar se uma frase é um palíndomo.(uma frase que mesmo invertida se ler da mesma forma)

---
+++ mundo_2/desafio_053.py
@@ -0,0 +1,9 @@
+# Crie um programa que leia uma frase qualquer e diga se é um palíndomo, desconsiderando os espaços. Ex: APOS A SOPA.
+
+frase_digitada_pelo_usuario = input('Digite uma frase: ').strip()
+formatar_frase = frase_digitada_pelo_usuario.replace(' ','')
+if formatar_frase[::-1] == formatar_frase:
+    print(f'A frase: {frase_digitada_pelo_usuario} é um palíndomo!')
+else:
+    print(f'A frase: {frase_digitada_pelo_usuario} não é um palíndomo!')
+
