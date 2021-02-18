a8f95f9fd0734e7b20bad6db77eb41c0d4af9b5e
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-18 07:50:22 -0300

    Alteração do desafio 047.
    Eu alterei seguindo a dica do professor, pois assim ele faz o mesmo trabalho sem consumir muito o processador.
    Segundo ele, quando coloca um if junto com um range, está fazendo duas vezes a mesma coisa.
    Também eu errei na parte do range, o programa estava indo até 48.

--- mundo_2/desafio_047.py
+++ mundo_2/desafio_047.py
@@ -1,8 +1,7 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
 from time import sleep
-for contador in range(1,50):
-    if contador % 2 == 0:
-        print(f'Estou no número: {contador}')
+for contador in range(2,51,2):
+        print(f'{contador}.',end=' ')
         sleep(1)
 
