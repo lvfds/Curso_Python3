fc46753ebd743cfb62e2d34b5cb5882c7059645a
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-18 09:36:04 -0300

    Correção desafio 055.
    Segundo o professor, colocar -1 e 999999 é uma gambiarra.
    Por isso eu alterei para seguir a dica dele, de quando for o primeiro, as duas variáveis resceberem o valor.

--- mundo_2/desafio_055.py
+++ mundo_2/desafio_055.py
@@ -1,11 +1,14 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-maior_peso = -1
-menor_peso = 99999999999999999999
+maior_peso = 0
+menor_peso = 0
 
 for contador in range(1,6):
     peso_digitado_pelo_usuario = float(input(f'Digite o peso do {contador}° usuário: '))
-    if peso_digitado_pelo_usuario > maior_peso:
+    if contador == 1:
+        maior_peso = peso_digitado_pelo_usuario
+        menor_peso = peso_digitado_pelo_usuario
+    elif peso_digitado_pelo_usuario > maior_peso:
         maior_peso = peso_digitado_pelo_usuario
     elif peso_digitado_pelo_usuario < menor_peso:
         menor_peso = peso_digitado_pelo_usuario
