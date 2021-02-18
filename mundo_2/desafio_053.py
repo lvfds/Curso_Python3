f35689ce6b051414ed3350a73c3df54bd6ab0dd4
Lucas Fontinele <78350661+lvfds@users.noreply.github.com>
2021-02-18 09:11:35 -0300

    Resolvendo o bug do exercício 053.
    Quando tinha uma letra maiúscula mesmo se fosse uma frase palíndomo dava erro, por isso no processo de formatação coloquei para transformar tudo em minúscula,

--- mundo_2/desafio_053.py
+++ mundo_2/desafio_053.py
@@ -1,7 +1,7 @@
 # Crie um programa que leia uma frase qualquer e diga se é um palíndomo, desconsiderando os espaços. Ex: APOS A SOPA.
 
 frase_digitada_pelo_usuario = input('Digite uma frase: ').strip()
-formatar_frase = frase_digitada_pelo_usuario.replace(' ','')
+formatar_frase = frase_digitada_pelo_usuario.replace(' ','').lower()
 if formatar_frase[::-1] == formatar_frase:
     print(f'A frase: {frase_digitada_pelo_usuario} é um palíndomo!')
 else:
